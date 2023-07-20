31eb2db9622db78c363c8a215d0f41209fbaaf79
Axel Noel Cabezas Cerda <114128602+AxelCab17@users.noreply.github.com>
2023-07-19 20:31:54 -0600

    Finalizacion semana 10

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -1,16 +1,19 @@
 
 package com.Tienda.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    
+
+            
     @RequestMapping("/")
-    public String page(Model model) {
+    public String page(Model model, HttpSession session) {
         model.addAttribute("mensaje", "Hola desde el controlador");
+
         return "index";
     }
     
